2834fac9f0010e641194888cc5af2d2cd649b974
Raúl Mullor Cots <rmulcot+01@upv.edu.es>
2025-05-23 19:36:53 +0200

    Servidor accesible desde cualquier wifi con ngrok, para que funcione hay que iniciar ngrok y el servidor sustutiur la API_BASE_URL por la url que te de ngrok.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'; // Keep As
 // Define la URL base de tu backend (Still needed for other potential uses or just as a constant)
 // ¡CAMBIA ESTO POR LA URL DE TU SERVIDOR DE PRODUCCIÓN CUANDO DESPLIEGUES!
 // Asegúrate de que esta IP es accesible desde tu dispositivo/simulador
-const API_BASE_URL = 'http://192.166.1.142:3001'; // <-- Usar la misma URL que en otras pantallas
+const API_BASE_URL = 'https://da05-2a0c-5a82-c002-1600-3d72-e0af-55ea-6c0f.ngrok-free.app'; // <-- Usar la misma URL que en otras pantallas
 
 // Define la clave para AsyncStorage (Still needed as a constant)
 const AUTH_TOKEN_KEY = 'userToken'; // Clave para guardar el token JWT
